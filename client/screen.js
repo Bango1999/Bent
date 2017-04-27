6a9e96d070722526f5a08405ce76eb35feb10a05
Lewis Brown <lcb005@uark.edu>
2017-04-27 18:42:03 -0500

    Updated logic to be pretty good, but I think I'm going to migrate back to 3d with block characters instead

--- client/screen.js
+++ client/screen.js
@@ -41,30 +41,55 @@
   character.position.y = 10;
   character.position.w = 5;
   character.position.h = 5;
+  character.stat = {};
+  character.stat.health = 200;
+  character.stat.maxHealth = 200;
 
   //map
 
   map = [
     {
-      item:'wall',
+      nid:'wall#1',
       x:0,
       y:0,
       w:100,
       h:5
     },
     {
-      item:'wall',
+      nid:'wall#1',
       x:0,
       y:70,
       w:100,
       h:5
     },
     {
-      item:'wall',
+      nid:'wall#1',
       x:0,
       y:5,
       w:5,
       h:65
+    },
+    {
+      nid:'npc#1',
+      x:50,
+      y:50,
+      w:5,
+      h:5
+    }
+  ];
+
+  style = [
+    {
+      nid:'wall#1',
+      innerColor:'#000',
+      outerColor:'#000',
+      type:'fillRect'
+    },
+    {
+      nid:'npc#1',
+      innerColor:'#fff',
+      outerColor:'#000',
+      type:'strokeRect'
     }
   ];
 
@@ -93,6 +118,24 @@
   {
     kd.tick();
   });
+
+  container.addEventListener('click', function(event)
+  {
+    mcx = event.clientX;
+    mcy = event.clientY;
+    console.log(mcx, mcy);
+  });
+
+  function findStyle(mapObject)
+  {
+    for(i in style)
+    {
+      if(mapObject.nid == style[i].nid)
+      {
+        return style[i];
+      }
+    }
+  }
 
   function testMove(inputX, inputY)
   {
@@ -125,7 +168,7 @@
   {
     ctx.clearRect(0, 0, width, height);
 
-    ctx.beginPath();
+    //render map
 
     for(i in map)
     {
@@ -142,23 +185,49 @@
       dh = (unit.y * map[i].h);
 
       //if map item is inside canvas, render it
-      if (cx < dx + dw &&
-          cx + cw > dx &&
-          cy < dy + dh &&
-          cy + ch > dy)
+      if(cx < dx + dw && cx + cw > dx && cy < dy + dh && cy + ch > dy)
       {
-        ctx.fillRect(dx, dy, dw, dh);
-        ctx.stroke();
+        setStyle = findStyle(map[i]);
+
+        if(setStyle == undefined)
+        {
+          console.log('could not find a set style for ' + map[i].name)
+        }
+        else
+        {
+          if(setStyle.hasOwnProperty('innerColor'))
+          {
+            ctx.fillStyle = setStyle.innerColor;
+          }
+
+          if(setStyle.hasOwnProperty('outerColor'))
+          {
+            ctx.strokeStyle = setStyle.outerColor;
+          }
+
+          if(setStyle.hasOwnProperty('type'))
+          {
+            if(setStyle.type == 'fillRect')
+            {
+              ctx.fillRect(dx, dy, dw, dh);
+            }
+            else if(setStyle.type == 'strokeRect')
+            {
+              ctx.strokeRect(dx, dy, dw, dh);
+            }
+          }
+        }
       }
     }
 
-    ctx.rect(
+    //render character
+
+    ctx.strokeRect(
       (width / 2) - (character.position.w / 2) + 0.5,
       (height / 2) - (character.position.h / 2) + 0.5,
       (unit.x * character.position.w),
       (unit.y * character.position.h)
     );
-    ctx.stroke();
   }
 
   window.setInterval(render, 1000 / 60);
