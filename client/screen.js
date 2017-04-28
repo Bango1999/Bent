cb17eb983afe1c211c8b46741922686fac307216
Lewis Brown <lcb005@uark.edu>
2017-04-28 12:51:05 -0500

    local updates with beginning to implement map spawn system + collision detection

--- client/screen.js
+++ client/screen.js
@@ -21,26 +21,47 @@
   camera.position.z = 7;
   character.add(camera);
 
-  // var grid = new THREE.GridHelper(10, 10);
-  // grid.rotation.x = Math.PI / 2;
-  // scene.add(grid);
+  var grid = new THREE.GridHelper(10, 10);
+  grid.rotation.x = Math.PI / 2;
+  grid.position.z = -0.49;
+  scene.add(grid);
 
-  for(x=-5;x<=5;x++)
+  var map = [
+    {x:0,y:5,z:0,w:11,h:1,d:1},
+    {x:5,y:0,z:0,w:1,h:11,d:1},
+    {x:0,y:-5,z:0,w:11,h:1,d:1},
+    {x:-5,y:0,z:0,w:1,h:11,d:1},
+  ];
+
+  for(z in map)
   {
-    for(y=-5;y<=5;y++)
-    {
-      if(x==-5 || x==5 || y==-5 || y==5)
-      {
-        wall = new THREE.Mesh(cube, new THREE.MeshLambertMaterial({color: 0xffffff}));
+    temp = new THREE.Mesh(
+      new THREE.BoxGeometry(map[z].w, map[z].h, map[z].d),
+      new THREE.MeshLambertMaterial({color: 0xffffff})
+    );
 
-        wall.position.x = x;
-        wall.position.y = y;
-        wall.receiveShadow = true;
-
-        scene.add(wall);
-      }
-    }
+    temp.position.set(map[z].x, map[z].y, map[z].z);
+    temp.receiveShadow = true;
+    scene.add(temp);
   }
+
+  // //demo map
+  // for(x=-5;x<=5;x++)
+  // {
+  //   for(y=-5;y<=5;y++)
+  //   {
+  //     if(x==-5 || x==5 || y==-5 || y==5)
+  //     {
+  //       wall = new THREE.Mesh(cube, new THREE.MeshLambertMaterial({color: 0xffffff}));
+  //
+  //       wall.position.x = x;
+  //       wall.position.y = y;
+  //       wall.receiveShadow = true;
+  //
+  //       scene.add(wall);
+  //     }
+  //   }
+  // }
 
   floor = new THREE.BoxGeometry(9, 9, 1);
   floorMaterial = new THREE.MeshLambertMaterial({color: 0xe67e22});
@@ -52,7 +73,6 @@
   light = new THREE.PointLight(0xffee88, 1.5, 10);
   light.position.set(0, 0, 1);
   light.castShadow = true;
-  light.shadowDarkness = 0.25;
   scene.add(light);
 
   light = new THREE.AmbientLight(0x404040);
@@ -66,13 +86,23 @@
 
   render();
 
+  window.addEventListener('resize', function()
+  {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    console.log(character);
+  });
+
   var kd = require('keydrown');
 
   var moveSpeed = 0.075;
 
   kd.Q.down(function()
   {
-    character.rotation.z += moveSpeed / 2;
+    character.rotateZ(moveSpeed / 2);
   });
   kd.W.down(function()
   {
@@ -80,7 +110,7 @@
   });
   kd.E.down(function()
   {
-    character.rotation.z -= moveSpeed / 2;
+    character.rotateZ(-moveSpeed / 2);
   });
   kd.A.down(function()
   {
