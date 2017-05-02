793746846f7fa11f2ea0e93bfa6c4fe2e5cdd39e
ramity <lcb005@email.uark.edu>
2017-05-02 16:38:09 -0500

    updated reference to three.js for linux support

--- client/screen.js
+++ client/screen.js
@@ -1,6 +1,6 @@
 (function() {
   var canvas = document.getElementById('container');
-  var THREE = require('THREE');
+  var THREE = require('three');
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 100);
   var renderer = new THREE.WebGLRenderer({
